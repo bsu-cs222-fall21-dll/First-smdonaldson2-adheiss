4b2ba091f1ce3aca40744823cba1a094cff2c4d1
DOWHEISS <drewdheiss@gmail.com>
2021-09-21 16:14:46 -0400

    Created UserInput test and production class.

--- src/main/java/cs222/bsu/edu/wikipedia/UserInput.java
+++ src/main/java/cs222/bsu/edu/wikipedia/UserInput.java
@@ -1,4 +1,11 @@
 package cs222.bsu.edu.wikipedia;
 
+import java.util.Scanner;
+
 public class UserInput {
+    public String getInput(){
+        Scanner input =  new Scanner(System.in);
+        System.out.println("Please Enter Your Search:");
+        return input.nextLine();
+    }
 }
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,10 +1,21 @@
+import cs222.bsu.edu.wikipedia.UserInput;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 
 public class SampleTest {
 
     @Test
-    public void test()
+    public void testUserInput()
     {
+        UserInput userInput = new UserInput();
+        String input = "Hello";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Assertions.assertEquals("Hello",userInput.getInput());
 
     }
 }
