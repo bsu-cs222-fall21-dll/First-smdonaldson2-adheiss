4b87ae34e72c34133f7fd530ee0c76d85c1c9cd0
DOWHEISS <drewdheiss@gmail.com>
2021-09-21 16:41:57 -0400

    GetJSONData Class added with URL conenction

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -16,5 +16,6 @@ public class SampleTest {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         Assertions.assertEquals("Hello",userInput.getInput());
+
     }
 }
