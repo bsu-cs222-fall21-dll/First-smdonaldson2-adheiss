24dace2d6881d5c6dc64da078f0d6c0a4208bf2c
smdonaldson2 <smdonaldson2@bsu.edu>
2021-09-26 21:19:16 -0400

    Wrote test user input class.

--- src/test/java/TestGetJSONData.java
+++ src/test/java/TestGetJSONData.java
@@ -7,11 +7,12 @@ import java.net.URL;
 
 public class TestGetJSONData {
     @Test
-    public void TestGetJSONData() throws MalformedURLException {
+    public void TestGettingJSONData() throws MalformedURLException {
         GetJSONData getJSONData = new GetJSONData();
 
         URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Frank_Zappa&rvprop=timestamp%7Cuser%7Ccomment&rvlimit=30");
         Assertions.assertEquals("{\"continue\":{\"rvcontinue\":\"20210919005429|1045132628\",\"continue\":\"||\"},\"query\":{\"normalized\":[{\"from\":\"Frank_Zappa\",\"to\":\"Frank Zappa\"}],\"pages\":{\"10672\":{\"pageid\":10672,\"ns\":0,\"title\":\"Frank Zappa\",\"revisions\":[{\"user\":\"75.172.213.218\",\"anon\":\"\",\"timestamp\":\"2021-09-19T01:09:23Z\",\"comment\":\"fix numbers\"}]}}}}", getJSONData.getSiteData(url));
 
 
+    }
 }
--- src/test/java/TestUserInput.java
+++ src/test/java/TestUserInput.java
@@ -1,6 +1,27 @@
+import cs222.bsu.edu.wikipedia.GetJSONData;
+import cs222.bsu.edu.wikipedia.Revision;
+
+import cs222.bsu.edu.wikipedia.RevisionFinder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class TestUserInput {
-    public void testUserInput(){
+    private Scanner scanner = new Scanner(System.in);
+
+
+    @Test
+    public void testUserInput() {
+        String testInputString;
+        System.out.println("Enter in the term you want to get data for: ");
+        System.out.println("\n");
+        testInputString = scanner.nextLine();
+        Assertions.assertEquals("Frank Zappa", testInputString);
 
     }
-
 }
