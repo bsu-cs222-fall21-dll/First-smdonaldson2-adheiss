a8f4af42c5e5e4a093b7c0f115a7927fd18e12db
DOWHEISS <drewdheiss@gmail.com>
2021-10-04 17:17:36 -0400

    Made revisions see-able in listview

--- src/main/java/cs222/bsu/edu/wikipedia/Controller.java
+++ src/main/java/cs222/bsu/edu/wikipedia/Controller.java
@@ -10,13 +10,13 @@ import java.net.URL;
 
 public class Controller {
     @FXML
-    private final ListView<String> list = new ListView<String>();
+    private ListView<String> list;
     @FXML
     private Button bInputSave = new Button("Save");
     @FXML
     private TextField input;
     @FXML
-    private ChoiceBox<String> choiceBox = new ChoiceBox<String>();
+    private ChoiceBox<String> choiceBox = new ChoiceBox<>();
 
     public void Search() throws MalformedURLException, URISyntaxException {
         String input = buttonClick();
@@ -24,21 +24,38 @@ public class Controller {
         String[] searchResult = displaySearchResults.searchData(input);
         populateChoiceBox(searchResult);
     }
-    public void getRevisions(){
-        System.out.println(ConsoleColors.PURPLE + "Redirected you to: " + searchResult + ConsoleColors.RESET);
+    public void getRevisions() throws MalformedURLException, URISyntaxException {
+        System.out.println("getting json revision");
+        String searchResult = getChoice(choiceBox);
         URL url = new URL("https://www.wikipedia.org/w/api.php");
         GetJSONRevision getJSONData = new GetJSONRevision();
         URL newUrl = GetJSONRevision.concatenate(url, searchResult);
         String json = getJSONData.getSiteData(newUrl);
 
-        DisplayRevisionData displayRevisionData = new DisplayRevisionData();
-        displayRevisionData.revisionData(json);
+        populateListView(json);
     }
+    public void populateListView(String json){
+        Finder finder = new Finder();
+        System.out.println("filling revision");
+        for(int i = 0;i<30;i++) {
+            Revision revision = finder.getRevisionFromJson(json).get(i);
+            String user = revision.user;
+            String comment = revision.comment;
+            String timestamp = revision.timestamp;
+            list.getItems().addAll(user, comment, timestamp);
+        }
+    }
+
     public String buttonClick(){
         bInputSave.setDisable(true);
         return input.getText();
     }
+    public String getChoice(ChoiceBox<String> choiceBox){
+        System.out.println("choice box choice");
+        return choiceBox.getValue();
+    }
     public void populateChoiceBox(String[] input){
+        System.out.println("filling choice box");
         for (String s : input) {
             choiceBox.getItems().add(s);
         }
